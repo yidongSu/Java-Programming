89fa47028f2351cde415e9ae99186592098d91ef
苏屹东 <2135445331@qq.com>
2024-12-16 09:03:20 +0800

    Add files via upload

---
+++ 202332110221week15/TestCourse.java
@@ -0,0 +1,20 @@
+public class TestCourse {
+public static void main(String[] args) {
+Course course1 = new Course("Data Structures");
+Course course2 = new Course("Database Systems");
+course1.addstudent("Peter Jones");
+course1.addstudent("Brian Smith");
+course1.addstudent("Anne Kennedy");
+
+course2.addstudent("Peter Jones");
+course2.addstudent("Steve Smith");
+System.out.println("Number of students in course1:"
++ course1. getNumberOfStudents());
+String[] students = course1.getStudents();
+for (int i =0; i < course1.getNumberOfStudents();i++)
+System.out.print(students[i] + ",");
+  System.out.println();
+System.out.print("Number of students in course2:"
++ course2. getNumberOfStudents());
+}
+}
